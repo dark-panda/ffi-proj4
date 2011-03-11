433c70d3eb0797d9b79a80d4c3feaeddda09355b
J Smith <dark.panda@gmail.com>
2011-03-11 16:53:49 -0500

    Display the PROJ_LIB value being tested against.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,7 @@ puts "PROJ version #{Proj4.version}"
 if defined?(Proj4::FFIProj4)
   puts "Using #{Array(Proj4::FFIProj4.proj4_library_path).join(', ')}"
 end
+puts "Using PROJ_LIB #{ENV['PROJ_LIB']}"
 
 module TestHelper
   TOLERANCE = 0.00000001
