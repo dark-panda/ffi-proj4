b6b116c89c51d94166af1a1425c5f24aed6cdc00
J Smith <dark.panda@gmail.com>
2011-03-18 17:55:14 -0400

    Fix JRuby handling of doubles.

--- lib/projxy.rb
+++ lib/projxy.rb
@@ -10,8 +10,17 @@ module Proj4
       if args.first.is_a?(FFI::Pointer)
         super(*args)
       else
-        self[:x] = args.first.to_f
-        self[:y] = args[1].to_f
+        self[:x] = if args.first.respond_to?(:read_double)
+          args.first.read_double
+        else
+          args.first.to_f
+        end
+
+        self[:y] = if args[1].respond_to?(:read_double)
+          args.first.read_double
+        else
+          args[1].to_f
+        end
       end
     end
 
